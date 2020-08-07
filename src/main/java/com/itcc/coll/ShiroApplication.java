1aad656ee8cb4c7affca53c9e0fe277901481403
NOPSMILE <153223193@qq.com>
2020-08-07 17:35:20 +0800

    v1.0

--- src/main/java/com/itcc/coll/ShiroApplication.java
+++ src/main/java/com/itcc/coll/ShiroApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.itcc.shiro.mapper")
+@MapperScan("com.itcc.coll.mapper")
 public class ShiroApplication {
 
     public static void main(String[] args) {
