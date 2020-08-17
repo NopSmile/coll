baa92410d89954637e83eadcf8cd31bc0b5efd91
NOPSMILE <153223193@qq.com>
2020-08-17 16:06:08 +0800

    添加taskid

--- src/main/java/com/itcc/coll/mapper/PermissionMapper.java
+++ src/main/java/com/itcc/coll/mapper/PermissionMapper.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface PermissionMapper {
 
 
-    @Select("select p.id as id, p.name as name, p.url as url from  coll_role_permission rp " +
+    @Select("select p.id as id, p.name as name, p.url as url,p.task_id as taskid from  coll_role_permission rp " +
             "left join coll_permission p on rp.permission_id=p.id " +
             "where  rp.role_id= #{roleId} ")
     List<Permission> findPermissionListByRoleId(@Param("roleId") int roleId);
